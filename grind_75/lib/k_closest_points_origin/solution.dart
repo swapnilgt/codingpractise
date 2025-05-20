376642b23e4d87795d8e4a2593445c2ef156d7b4
Swapnil <swapnilgupta.iiith@gmail.com>
2025-05-20 22:30:34 +0530

    Solution for k closest points

--- grind_75/lib/k_closest_points_origin/solution.dart
+++ grind_75/lib/k_closest_points_origin/solution.dart
@@ -1,45 +1,72 @@
 import 'dart:math';
 
+// import 'package:flutter/material.dart';
+
 class Solution {
   List<List<int>> kClosest(List<List<int>> points, int k) {
     if (k == points.length) {
       return points;
     }
 
+    debugPrint("!!! Distance pre sort: !!!");
+    _printPoints(points);
+
     // Getting the last non-left node.
     for (int i = (points.length / 2).floor() - 1; i >= 0; i--) {
       _heapify(points, points.length, i);
+      // Printing post first iteration.
+      debugPrint("Post i = $i");
+      _printPoints(points);
     }
+
+    debugPrint("### Distance post sort: ###");
+    _printPoints(points);
+
+    // Return value.
     final List<List<int>> retVal = [];
-    for (int i = k - 1; i >= 0; i--) {
-      retVal.add(points[i]);
+
+    //debugPrint("## heap: $points");
+
+    // Taking the top k smallest items.
+    for (int i = 1; i <= k; i++) {
+      retVal.add(points[0]);
+      points[0] = points[points.length - i];
+      _heapify(points, points.length - i, 0);
     }
     return retVal;
+  }
+
+  void _printPoints(List<List<int>> points) {
+    final List<double> distance = [];
+    for (int i = 0; i < points.length; i++) {
+      distance.add(_getDistance(points[i]));
+    }
+    debugPrint("$distance");
   }
 
   void _heapify(List<List<int>> points, int n, int i) {
     int left = i * 2 + 1;
     int right = i * 2 + 2;
-    int largest = i;
+    int smallest = i;
 
     // Check with the left node.
     if (left < n &&
-        _getDistance(points[left]) > _getDistance(points[largest])) {
-      largest = left;
+        _getDistance(points[left]) < _getDistance(points[smallest])) {
+      smallest = left;
     }
 
     // Check with the right.
     if (right < n &&
-        _getDistance(points[right]) > _getDistance(points[largest])) {
-      largest = right;
+        _getDistance(points[right]) < _getDistance(points[smallest])) {
+      smallest = right;
     }
 
     // Swapping if needed.
-    if (largest != i) {
-      final List<int> temp = points[largest];
-      points[largest] = points[i];
+    if (smallest != i) {
+      final List<int> temp = points[smallest];
+      points[smallest] = points[i];
       points[i] = temp;
-      _heapify(points, n, i);
+      _heapify(points, n, smallest);
     }
   }
 
